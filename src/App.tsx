3abb429e1e6fb40bcba9f215adc78212a150bc96
GabrielGladykowski <gabriel.gladki2003@gmail.com>
2024-03-30 21:56:00 +0100

    chore: install query to menage state and api request

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,18 @@ const globalStyles = css`
 `;
 
 function App() {
+  // const base = "https://api.stackexchange.com/";
+  // fetch(`${base}2.3/tags?order=desc&sort=popular&site=stackoverflow`)
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Response is not okay");
+  //     }
+
+  //     return response.json();
+  //   })
+  //   .then((data) => console.log(data))
+  //   .catch((error) => console.error(error));
+
   return (
     <StyledEngineProvider injectFirst>
       <Global styles={globalStyles} />
